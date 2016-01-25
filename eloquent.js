451e79b3fb16e9b5c6f74ec2fcfcfd1c9adf342c
Someone Special <someone@devacademy.co.nz>
2016-01-26 10:26:05 +1300

    Fix indentation errors

--- eloquent.js
+++ eloquent.js
@@ -1,57 +1,57 @@
-//1.Minimum
+// 1.Minimum
 
-
-var minimum = function(x,y){
-  if (x>y) {
-    return y;
+var minimum = function (x, y) {
+  if (x > y) {
+    return y
   }
   else {
-    return x;
+    return x
   }
 }
 
-//2.Recursion
+// 2.Recursion
 
-function isEven(number) {
+function isEven (number) {
+  if (number === 0) {  // zero is even
+    return (true)
 
-if(number === 0){ //zero is even
-  return (true);
-
-if(number === 1){ //one is odd
-  return (false);
+    if (number === 1) {  // one is odd
+      return (false)
+    }
+  else if (number < 0) {
+    return (isEven(number + 2))  // number less than zero
+  }
+     else {
+    return (isEven(number - 2))   // number greater than zero
+  }
+  }
 }
-else if(number<0){
-  return (isEven(number+2));  //number less than zero
-}
-else {
-  return (isEven(number-2));  //number greater than zero
-}
-}
+isEven()
 
-//3.Bean Counting
+// 3.Bean Counting
 
-function countBs(sentence){
-
+function countBs (sentence) {
   var count = 0;
 
-for (var i=0;i<sentence.length;i++){
-  if(sentence.charAt(i) === "B") {
+  for (var i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) === 'B') {
       count++;
-  }  
-}
-return count;
-}
+    }
+  }
 
-////////////////////////////////
-
-function countChar(sentence,letter){
-
-var count = 0;
-
-for (var i=0;i<sentence.length;i++){
-  if(sentence.charAt(i) === letter) {
-      count++;
-     }
-}
   return count;
 }
+countBs()
+
+function countChar (sentence, letter) {
+  var count = 0;
+
+  for (var i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) === letter) {
+      count++;
+    }
+  }
+
+  return count;
+}
+countChar()
