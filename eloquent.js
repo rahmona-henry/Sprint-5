36125ffa3c40839be4f40fb3486259c595655271
Someone Special <someone@devacademy.co.nz>
2016-01-26 09:56:15 +1300

    Finish Char function

--- eloquent.js
+++ eloquent.js
@@ -1,5 +1,5 @@
-//MinimumRecursion
-//1.Write a function min that takes two arguments and returns their minimum.
+//1.Minimum
+
 
 var minimum = function(x,y){
   if (x>y) {
@@ -8,25 +8,16 @@ var minimum = function(x,y){
   else {
     return x;
   }
-};
+}
 
-//Recursion
-//Zero is even.
-//One is odd.
-//For any other number N, its evenness is the same as N - 2.
-//2.Define a recursive function isEven corresponding to this description. The function should accept
-//a number parameter and return a Boolean.
-//Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
-//We call isEven(number -2) to go back to the top of the function with the number that was 
-//inputed intially but 2 less than before and it will keep doing that until the number is 1 or 0
-//and then it will be able to return the boolean true or false (even or odd).
-//Dealing with negetive numbers, we could use num = Math.abs(num);to ensure that whatever number gets passed is ablsolute
+//2.Recursion
+
 function isEven(number) {
 
 if(number === 0){ //zero is even
   return (true);
 
-else if(number === 1){ //one is odd
+if(number === 1){ //one is odd
   return (false);
 }
 else if(number<0){
@@ -35,37 +26,32 @@ else if(number<0){
 else {
   return (isEven(number-2));  //number greater than zero
 }
-};
+}
 
-//Bean Counting
-//Write a function countBs that takes a string as its only argument and returns a number that 
-//indicates how many uppercase “B” characters are in the string.
-//Next, write a function called countChar that behaves like countBs, except it takes a second
-//argument that indicates the character that is to be counted (rather than counting only uppercase
-// “B” characters). Rewrite countBs to make use of this new function.
+//3.Bean Counting
 
 function countBs(sentence){
 
   var count = 0;
 
-for (var i=0;i<=sentence.length;i++){
-  if(sentence.charAt === "B") {
+for (var i=0;i<sentence.length;i++){
+  if(sentence.charAt(i) === "B") {
       count++;
   }  
 }
 return count;
-};
+}
+
 ////////////////////////////////
 
 function countChar(sentence,letter){
 
 var count = 0;
 
-for (var i=0;i<=sentence.length;i++){
-  if(sentence.charAt === letter) {
+for (var i=0;i<sentence.length;i++){
+  if(sentence.charAt(i) === letter) {
       count++;
      }
 }
   return count;
-
-};
+}
